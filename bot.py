c2f538b98b7889f2c149cdeaf685a9b8b407f107
Gagan T K <gagantk07@gmail.com>
2020-05-13 21:09:06 +0530

    third commit

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 import os
 
 api_id = int(os.environ.get('API_ID', 6))
 api_hash = os.environ.get('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
 FROMM = int(os.environ.get('FROMM', 10))
 TO = int(os.environ.get('TO', 9))
-client = TelegramClient('sess', api_id, api_hash)
+HU_STRING_SESSION = os.environ.get('HU_STRING_SESSION', None)
+client = TelegramClient(StringSession(HU_STRING_SESSION), api_id, api_hash)
 client.start()
 
 @client.on(events.NewMessage(chats=FROMM))
