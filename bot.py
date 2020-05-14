cfe7c2b18fc0a48c2241c736e6ef3974da92daf2
Gagan T K <gagantk07@gmail.com>
2020-05-14 16:23:30 +0530

    Seventh commit

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ import os
 api_id = int(os.environ.get('API_ID', 6))
 api_hash = os.environ.get('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
 FROMM = os.environ.get('FROMM')
+print(FROMM)
+print(type(FROMM))
 FROMM_LIST = [int(id) for id in FROMM.split(', ')]
+print(FROMM_LIST)
 TO = int(os.environ.get('TO', 10))
 HU_STRING_SESSION = os.environ.get('HU_STRING_SESSION', None)
 client = TelegramClient(StringSession(HU_STRING_SESSION), api_id, api_hash)
